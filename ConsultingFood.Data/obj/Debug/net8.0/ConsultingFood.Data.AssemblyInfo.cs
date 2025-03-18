32164dd9bc720c3c52fa8e9ae83c3d89bfb04178
djordjevukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-03-18 15:49:44 +0100

    icons added

--- ConsultingFood.Data/obj/Debug/net8.0/ConsultingFood.Data.AssemblyInfo.cs
+++ ConsultingFood.Data/obj/Debug/net8.0/ConsultingFood.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsultingFood.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1475c17c9cc2df8132bcb84bbf655d8b6d2e46c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsultingFood.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsultingFood.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
