0c550c8cffd08ca6d119b0fb16b8f6ce303fae55
djordjevukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-03-18 18:23:35 +0100

    some small changes

--- ConsultingFood/Controllers/HomeController.cs
+++ ConsultingFood/Controllers/HomeController.cs
@@ -23,10 +23,9 @@ namespace ConsultingFood.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Reviews()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
