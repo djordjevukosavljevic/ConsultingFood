1db8edbe2b49b2db3cf1bfc56c42c0fdb75e08d8
Djordje Vukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-04-15 18:32:09 +0200

    minor changes

---
+++ ConsultingFood.Data/Review.cs
@@ -0,0 +1,13 @@
+namespace ConsultingFood.Data
+{
+    public class Review
+    {
+        public int Id {get; set;}
+
+        public required string Comment {get; set;}
+
+        public required double Grade {get; set;}
+
+        public DateTime CreatedAt {get; set;}
+    }
+}
