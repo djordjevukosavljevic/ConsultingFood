a932bb5450740c815c3284a5f3a356cc08c7249d
Djordje Vukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-05-15 21:18:37 +0200

    framework changes again...

--- ConsultingFood/obj/Debug/net8.0/ConsultingFood.AssemblyInfo.cs
+++ ConsultingFood/obj/Debug/net8.0/ConsultingFood.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsultingFood")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ac70e0a5a5c77fe279c82faa3c0496c8349ca0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f424606bbca10ac3aa3c50cc1222fb63f8ea6dc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsultingFood")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsultingFood")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
